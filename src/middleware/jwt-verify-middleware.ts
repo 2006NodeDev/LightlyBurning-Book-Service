932f6646450df3129275590a86566fb02d6283a7
Abatson <abatson94@gmail.com>
2020-08-06 20:05:04 -0400

    jwt middleware through jwks and auth0

--- src/middleware/jwt-verify-middleware.ts
+++ src/middleware/jwt-verify-middleware.ts
@@ -1,19 +1,17 @@
-import { Response, NextFunction} from "express";
-import jwt from 'jsonwebtoken'
+import jwt from 'express-jwt'
+import jwksRsa from 'jwks-rsa'
 
 
-export const JWTVerifyMiddleware = (req: any, res: Response, Next: NextFunction) => {
-    try {
-        // ?. operator is really just short hand for the guard operator
-        // req.headers.authorization && req.headers.authorization.split(' ').pop()
-        let token = req.headers.authorization?.split(' ').pop()//turn the string Bearer token -> token
-        if( token){
-            req.user = jwt.verify(token, 'thisIsASecret')
-        }
-        Next()//no token and no req.user
-        //other validation middleware will catch it
-    } catch (e) {
-        console.log(e);
-        Next(e)
-    }
-}
+
+
+export const JWTVerifyMiddleware = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://lightlyburning.us.auth0.com/.well-known/jwks.json`
+    }),
+    audience: `lightlyburning.js-army.com`,
+    issuer:'https://lightlyburning.us.auth0.com/',
+    algorithms:['RS256']
+})
